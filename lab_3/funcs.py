9c9280b20d23e2c60a3603d024ff9356af746a68
VladMaksimum <belyaninvladislav@gmail.com>
2026-01-30 20:08:22 +0200

    lab_3

--- lab_3/funcs.py
+++ lab_3/funcs.py
@@ -1,16 +1,26 @@
-def count_contr_bits(block: str, cnt_inds: list[int]) -> None:
+def count_contr_bits(block: str, cnt_inds: list[int]) -> str:
     for i in cnt_inds:
         cur_pos = i-1
         cnt = ''
 
         while cur_pos < len(block):
-            cnt += block[cur_pos:cur_pos+1:]
+            cnt += block[cur_pos : cur_pos + i:]
             cur_pos += (i*2)
         
-        block = block[:(i-1):] + str(cnt.count("1") % 2) + block[(i-1)::]
+        block = block[:(i-1):] + str(cnt.count("1") % 2) + block[(i)::]
+    return block
+
+def decode(block: str, cnt_inds: list[int]) -> str:
+    indexs = cnt_inds[::-1]
+    for i in indexs:
+        block = block[:(i-1):] + block[(i)::]
+    
+    res = ''
+    for j in range(0, len(block), 8):
+        res += chr(int(block[j:(j+8):], 2))
+    
+    return res
 
 if __name__ == "__main__":
-    block = '01010000011001010110111001110100'
-    count_contr_bits(block, [1,2,4,8,16,32])
-    print(block)
+    print(decode('11010011000001100010101101110011110100', [1,2,4,8,16,32]))
 
--- lab_3/task_1.py
+++ lab_3/task_1.py
@@ -1,3 +1,6 @@
+from funcs import count_contr_bits
+from funcs import decode
+
 
 word = "Pentium "
 bits = ''
@@ -9,11 +12,84 @@ for symbol in word:
     
     bits += bits_tmp
 
+print("Transform to bits:", bits)
 blocks = [bits[:32:], bits[32::]]
-print(blocks)
+print("Separate into 2 blocks:", blocks)
 contr_bits_indexs = [1,2,4,8,16,32]
 
 for i in contr_bits_indexs:
     blocks[0] = blocks[0][:(i-1):] + "0" + blocks[0][(i-1)::]
     blocks[1] = blocks[1][:(i-1):] + "0" + blocks[1][(i-1)::]
+print("Add control bits:", blocks)
+
+for j in range(len(blocks)):
+    blocks[j] = count_contr_bits(blocks[j], contr_bits_indexs)
+
+
+i1, i2 = 5, 21
+print("Count control bits",  blocks)
+
+
+if blocks[0][i1-1] == "1":
+    blocks[0] = blocks[0][:(i1-1):] + "0" + blocks[0][(i1)::]
+else:
+    blocks[0] = blocks[0][:(i1-1):] + "1" + blocks[0][(i1)::]
+
+if blocks[1][i2-1] == "1":
+    blocks[1] = blocks[1][:(i2-1):] + "0" + blocks[1][(i2)::]
+else:
+    blocks[1] = blocks[1][:(i2-1):] + "1" + blocks[1][(i2)::]
+
+message = blocks.copy()
+print("After breaking:",  blocks)
+
+for i in contr_bits_indexs:
+    blocks[0] = blocks[0][:(i-1):] + "0" + blocks[0][(i)::]
+    blocks[1] = blocks[1][:(i-1):] + "0" + blocks[1][(i)::]
+
+for j in range(len(blocks)):
+    blocks[j] = count_contr_bits(blocks[j], contr_bits_indexs)
+
+
+print("Recount bits:", blocks)
+
+broken_message = ''
+for block in blocks:
+    broken_message += decode(block, contr_bits_indexs)
+
+print("Broken message:", "<" + broken_message + ">")
+
+brk_ind = [0, 0]
+for k in range(len(message)):
+    for l in range(len(message[k])):
+        if message[k][l] != blocks[k][l]:
+            brk_ind[k] += (l + 1)
+
+print("Broken bits:", brk_ind)
+
+i3, i4 = brk_ind[0], brk_ind[1]
+
+if blocks[1][i4-1] == "1":
+    blocks[1] = blocks[1][:(i4-1):] + "0" + blocks[1][(i4)::]
+else:
+    blocks[1] = blocks[1][:(i4-1):] + "1" + blocks[1][(i4)::]
+
+if blocks[0][i3-1] == "1":
+    blocks[0] = blocks[0][:(i3-1):] + "0" + blocks[0][(i3)::]
+else:
+    blocks[0] = blocks[0][:(i3-1):] + "1" + blocks[0][(i3)::]
+
+print("Fix bits:", blocks)
+
+for i in contr_bits_indexs:
+    blocks[0] = blocks[0][:(i-1):] + "0" + blocks[0][(i)::]
+    blocks[1] = blocks[1][:(i-1):] + "0" + blocks[1][(i)::]
+
+for j in range(len(blocks)):
+    blocks[j] = count_contr_bits(blocks[j], contr_bits_indexs)
+
+print("Recount bits:", blocks)
+
+print("Decoded meassge:", "<" + decode(blocks[0], contr_bits_indexs) + decode(blocks[1], contr_bits_indexs) + ">")
+ 
 
